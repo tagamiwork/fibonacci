56f6078a0e3dbb203ecbbb60bf1afb4a645f2108
tagamiwork <uedawork2020@gmail.com>
2020-05-14 01:41:55 +0900

    指数オーダーのfibを実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+function fib(n) {
+    
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }// if-else
+    return fib(n - 1) + fib(n - 2);
+
+}// fib()
+
+const length = 40;
+for (let i = 1; i <= length; i++) {
+    console.log(i + '番目:' + fib(i));
+}// for
+
